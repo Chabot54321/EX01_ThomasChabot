2b91d5f067e6beb18b484ab0caca47e91e863c27
Thomas Chabot <thomas.chabot.1@ulaval.ca>
2013-09-11 08:07:01 -0400

    Update thomas_chabot_fizzbuzz.dart

--- bin/thomas_chabot_fizzbuzz.dart
+++ bin/thomas_chabot_fizzbuzz.dart
@@ -1,6 +1,8 @@
-
 main() {
   
+  
+  // Formules tirées du site :
+  //http://api.dartlang.org/docs/releases/latest/dart_math.html
   
 String FIZZ = 'Fizz';
 String BUZZ = 'Buzz';
@@ -9,18 +11,22 @@ String Sans_Gout = 'Sans goût';
 
     for (int i = 1; i <= 200; i ++) 
      
-    
-    {   bool is_Sans_Gout = i % 1 == 0;
-        bool is_fizz = i % 3 == 0;
-        bool is_buzz = i % 5 == 0;
+      //La formule booléenne est utilisé pour 1 = paramêtre respecté, 0 = sinon (ou vice versa, pas trop sur)
+      //Utilisation d'une boucle FOR pour les nombre 1 à 200
+      
+    { 
+        bool si_Sans_Gout = i % 1 == 0;
+        bool si_fizz = i % 3 == 0;
+        bool si_buzz = i % 5 == 0;
+      
         print("Regardons si les nombres 1 à 200 sont FizzBuzzant!");
-        print(   is_fizz && is_buzz ? "Le nombre  $i est FIZZ+BUZZ!!!" 
-                 :is_fizz? "Le nombre  $i est FIZZ" 
-                 :is_buzz ?"Le nombre  $i est BUZZ"  
-                 :is_Sans_Gout? "Le nombre $i n'a rien d'exceptionnel..."
-                 :i);             
+        print(               si_fizz && si_buzz ? "Le nombre  $i est FIZZ+BUZZ!!!" 
+                            :si_fizz? "Le nombre  $i est FIZZ" 
+                            :si_buzz ?"Le nombre  $i est BUZZ"  
+                            :si_Sans_Gout? "Le nombre $i n'a rien d'exceptionnel..."
+                            :i);
+              
     }
  
     return;
 }
-
